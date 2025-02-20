51ebda7f4e55c8bb0128f68545c8c64afc7d4e11
D-B-C__SR-DEVARAKONDA <balachandrududevarakonda3@gmail.com>
2025-02-20 23:09:16 +0530

    Update WelcomePage.tsx

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -106,7 +106,7 @@ export function WelcomePage({ user, onSignOut }: WelcomePageProps) {
             </div>
 
             <div>
-              <label className="block text-sm font-medium text-gray-700">Message for-leave college (Reason)</label>
+              <label className="block text-sm font-medium text-gray-700">Apply for leave (Reason)</label>
               <textarea
                 name="message"
                 rows={4}
